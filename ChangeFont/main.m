8764b4735c3e2d731f7ea3cd9cad0df7a6e5c50c
chenhongwei <chenhongwei@igancao.com>
2016-04-27 19:05:14 +0800

    Initial Commit

---
+++ ChangeFont/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChangeFont
+//
+//  Created by HenryCheng on 16/4/27.
+//  Copyright © 2016年 HenryCheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
